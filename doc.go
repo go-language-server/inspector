46c32b81dc525888dbfb21cacb7c449ceff1753f
Koichi Shiraishi <zchee.io@gmail.com>
2020-01-26 05:19:41 +0900

    inspector: add top level package synopsis.

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2020 The go-language-server Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package inspector implements Language Server Protocol Inspector specification in Go.
+package inspector
